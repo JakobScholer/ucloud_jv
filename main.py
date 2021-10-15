455efb2342b92adfbfbbd543e7f0564bce975242
Jakob Schøler <35096865+JakobScholer@users.noreply.github.com>
2021-10-15 11:08:14 +0200

    main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import os
+import shutil
 import sys
 from mod import smiles, graphGMLString
 
@@ -6,6 +8,51 @@ from src.root_mean_square import root_mean_square
 from src.runner import runner_main, make_cut_molecule, find_all_cuts, make_cut
 from src.generate_tree import generate_tree_main, reaction_and_product_to_gml, read_energy_profiles
 from src.mod_to_xyz import mod_to_xyz_main, mod_to_xyz
+
+
+def run_zstruct(outdir, offset):
+    os.chdir("zstruct")
+    os.system("./zstruct.exe")
+    _, _, filenames = next(os.walk("scratch"))
+    isomer_count = sum(fn.startswith("ISO") for fn in filenames)
+    os.chdir("..")
+
+    for i in range(isomer_count):
+        strid = str(i).zfill(4)
+        shutil.move(f"zstruct/scratch/ISOMERS{strid}", f"{outdir}/ISOMERS{str(i+offset).zfill(4)}")
+        shutil.move(f"zstruct/scratch/initial{strid}.xyz", f"{outdir}/initial{str(i+offset).zfill(4)}.xyz")
+
+    return isomer_count
+
+
+def prepare_zstruct(combination, molecules, dir_path):
+    assert(len(combination) <= 2)
+    os.system("rm -r ZStruct/scratch")
+    os.system("rm ZStruct/*.xyz")
+    os.makedirs('ZStruct/scratch')
+
+    for i, name in enumerate(combination):
+        m = molecules[name]
+        shutil.copy(f"{dir_path}/{m['xyz']}", f"zstruct/react{i+1}.xyz")
+        shutil.copy(f"{dir_path}/{m['frozen']}", f"zstruct/frozen{i+1}.xyz")
+
+
+def generate_isomers(path: str):
+    out_dir = "scratch/isomers"
+    if os.path.exists(out_dir):
+        os.system(f"rm -r {out_dir}")
+    os.mkdir("scratch/isomers")
+
+    with open(path) as f:
+        data = json.load(f)
+
+    dir_path = os.path.dirname(path)
+    offset = 0
+    for c in data["combinations"]:
+        prepare_zstruct(c, data["molecules"], dir_path)
+        offset += run_zstruct(out_dir, offset)
+    return offset
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -22,9 +69,10 @@ if __name__ == '__main__':
         mod_to_xyz(g, to_file=True)             # convert molecule for zstruct to understand it
 
         # run zstruct with molecule.xyz (molecule.frozen is empty for now)
+        generate_isomers("data/main_example.json")
         if not os.path.exists('ZStruct/scratch'):
             os.makedirs('ZStruct/scratch')
-
+        '''
         gml_string, atom_core, energy_curve = reaction_and_product_to_gml('src/stringfile.xyz0000', visualize=True)
         g = graphGMLString(gml_string)
         molecule, lookup_dict = make_cut_molecule(g, atom_core)
@@ -40,4 +88,5 @@ if __name__ == '__main__':
             ct = fi.readlines()
         curve = read_energy_profiles(ct)
         x = root_mean_square(energy_curve, curve)   # based on value decide if curve is the same
+        '''
 
