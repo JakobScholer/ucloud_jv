91caae525858f0a88a5ee01376e03a77cb6ff109
ValentinHoeck <valentin.hoeck@gmail.com>
2021-10-15 10:10:09 +0200

    Create cut_tree.py
    pytho script for generatinf cut trees and all needed methods (Soon to be doen)

---
+++ src/cut_tree.py
@@ -0,0 +1,49 @@
+from root_mean_square import root_mean_square
+from runner import runner_main, make_cut_molecule, find_all_cuts, make_cut
+from generate_tree import generate_tree_main, reaction_and_product_to_gml, read_energy_profiles
+from mod_to_xyz import mod_to_xyz_main, mod_to_xyz
+
+class CutTreeNode:
+    def __init__(self, cuts):
+        self.energy = []  # List for energy leves at different notes for the reaction
+        self.RMS = 0  # the root mean square base on the original molecule reaction
+        self.stringfile = ""  # The string file with the reaction, used for GML rule
+        self.cuts = cuts  # which cuts on the molecule was made
+        self.childs = []  # Childs made from the molecule
+
+class CutTree:
+    def __init__(self, cut_molecule, lookup_dict):
+        self.tree_layers = {} # Dictionary for each layer of the cut tree. Keys are ints of cuts made for a certain tree_layers
+        self.cut_molecule = cut_molecule # the molecule to perfom cuts on
+        self.cut_molecule_lookup_dict = lookup_dict # dict for cut_molecule
+
+# Functions i need
+
+# generate childs of a nodes
+
+# Check if child already exist
+
+# generate root node
+# input:
+    # Stringfile from Xtb
+def make_root(stringfile: string, visuals: boolean):
+    # make cut molecule and energy_curve
+    gml_string, atom_core, energy_curve = reaction_and_product_to_gml(stringfile, visualize=visuals)
+    g = graphGMLString(gml_string)
+    molecule, lookup_dict = make_cut_molecule(g, atom_core)
+    # generate cut_tree
+    ct = CutTree(molecule, lookup_dict)
+
+    # generate node for root
+    root = CutTreeNode(set())
+    root.energy = energy_curve
+
+    # Insert root in layers
+    ct.tree_layers[0] = [root]
+
+def main():
+    # for testing
+    'src/stringfile.xyz0000'
+
+if __name__ == "__main__":
+    main()
