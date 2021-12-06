2cffa0d826f7758186d8868f920f80e2325cdb07
ValentinHoeck <valentin.hoeck@gmail.com>
2021-12-06 10:08:22 +0100

    reactions to smiles update

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ if __name__ == '__main__':
         elif str(sys.argv[1]) == "zstruct_gsm":
             zstruct_gsm_main()
         elif str(sys.argv[1]) == "smiles_to_reactions":
-            print("we was here")
-            make_reactions("CCO")
+            make_reactions(["C=C(C)C(C(CC)CN(C(=O)OC(C)=O)C([O-])=NC(C)C(C=CC)C1CCCCC1)C2CCCCC2"])
         else:
             print("derp")
         #g = smiles("CCO")                       # molecule to test reaction on
--- src/smiles_to_reactions.py
+++ src/smiles_to_reactions.py
@@ -1,6 +1,10 @@
 from src.stringfile_tester import check_educt_to_product
-from rdkit.Chem import RWMol, AddHs, MolFromSmiles, MolToXYZBlock
+from rdkit.Chem import RWMol, AddHs, MolFromSmiles, MolToXYZBlock, rdDepictor
+from rdkit.Chem.AllChem import EmbedMolecule
 from os import walk, path, listdir
+from src.zstruct_and_gsm import run_zstruct_and_gsm
+
+# C=C(C)C(C(CC)CN(C(=O)OC(C)=O)C([O-])=NC(C)C(C=CC)C1CCCCC1)C2CCCCC2
 
 def make_reactions(smiles):# tag en smiles som input
 # GØR HELE BLACK BOX DELEN!
@@ -8,14 +12,17 @@ def make_reactions(smiles):# tag en smiles som input
     for string in smiles:
         mol = RWMol(MolFromSmiles(string)) # lav rdkitmol fra smiles string
         mol = AddHs(mol) # add hydrogen for good measure.
+        rdDepictor.Compute2DCoords(mol) # add coordinates with a comformer
+        EmbedMolecule(mol, randomSeed=0xf00d)
         xyz_list.append(MolToXYZBlock(mol)) # convert til xyz fil
 
     reaction_name = smiles[0]
     for i in range(1,len(smiles)):
         reaction_name = reaction_name + "_+_" + string
     # kør blackbox
-    #smiles_path = run_zstruct_and_gsm(xyz_list, smiles_string: reaction_name)
-    smiles_path = "test_folder/" # black box wannabe tester
+    smiles_path = run_zstruct_and_gsm(xyz_list, reaction_name)
+    print(smiles_path)
+    #smiles_path = "test_folder/" # black box wannabe tester
     stringfile_path = listdir(smiles_path)
     print(stringfile_path)
     reaction_folders = [smiles_path + s for s in stringfile_path]
@@ -23,9 +30,9 @@ def make_reactions(smiles):# tag en smiles som input
     for folder in reaction_folders: # gå over hver eneste stringfile+isomer og lav en cut dag
         containment = listdir(folder)
         if len(containment) > 1: # must be 2 files. if no stringfile was generated
-            stringfile = folder + "/" + containment[0]
+            stringfile = folder + "/" + containment[2]
             isomer_file = folder + "/" + containment[1]
             print("succes!")
-            if check_educt_to_product(stringfile): # if there is a reaction in the stringfile. make a cut dag!
-                print("with a top!")
+            #if check_educt_to_product(stringfile): # if there is a reaction in the stringfile. make a cut dag!
+                #print("with a top!")
                 #generate_cut_dag_main(stringfile, isomer_file, folder)
