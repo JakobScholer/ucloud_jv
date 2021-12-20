da844a5ef8d77ad67643ad58fc65489d0a319dfa
ValentinHoeck <valentin.hoeck@gmail.com>
2021-12-20 15:35:31 +0100

    smiles to reaction
    now takes new inputs and handle to run blackbox or read data

--- src/smiles_to_reactions.py
+++ src/smiles_to_reactions.py
@@ -7,23 +7,28 @@ from src.generate_cut_dag import generate_cut_dag_main, generate_cut_dag_2
 
 # C=C(C)C(C(CC)CN(C(=O)OC(C)=O)C([O-])=NC(C)C(C=CC)C1CCCCC1)C2CCCCC2
 
-def make_reactions(smiles):# tag en smiles som input
+# takes a mode and a string or list of strings as input
+    # mode 0 runs blackbox adn the list of stringfiles must the smiles for reactions
+    # mode 1 reads data from a folder. string_data must be the path to the already compiled cut dag data
+def make_reactions(mode: int, string_data, visual_cut_dag: bool=False, visual_stringfiles: bool=False):
 # GØR HELE BLACK BOX DELEN!
-    xyz_list = []
-    for string in smiles:
-        mol = RWMol(MolFromSmiles(string)) # lav rdkitmol fra smiles string
-        mol = AddHs(mol) # add hydrogen for good measure.
-        rdDepictor.Compute2DCoords(mol) # add coordinates with a comformer
-        EmbedMolecule(mol, randomSeed=0xf00d)
-        xyz_list.append(MolToXYZBlock(mol)) # convert til xyz fil
-    print(xyz_list[0])
-    reaction_name = smiles[0]
-    for i in range(1,len(smiles)):
-        reaction_name = reaction_name + "_+_" + string
-    # kør blackbox
-    #smiles_path = run_zstruct_and_gsm(xyz_list, reaction_name)
-    smiles_path = "xyz_test_files/derp"
-    #smiles_path = "test_folder/" # black box wannabe tester
+    if mode == 0:
+        xyz_list = []
+        for string in string_data:
+            mol = RWMol(MolFromSmiles(string)) # lav rdkitmol fra smiles string
+            mol = AddHs(mol) # add hydrogen for good measure.
+            rdDepictor.Compute2DCoords(mol) # add coordinates with a comformer
+            EmbedMolecule(mol, randomSeed=0xf00d)
+            xyz_list.append(MolToXYZBlock(mol)) # convert til xyz fil
+        #print(xyz_list[0])
+        reaction_name = smiles[0]
+        for i in range(1,len(string_data)):
+            reaction_name = reaction_name + "_+_" + string
+        # kør blackbox
+        smiles_path = run_zstruct_and_gsm(xyz_list, reaction_name)
+    elif mode == 1:
+        smiles_path = string_data
+
     stringfile_path = listdir(smiles_path)#
     reaction_folders = [smiles_path + "/" + s for s in stringfile_path]
 
@@ -39,7 +44,6 @@ def make_reactions(smiles):# tag en smiles som input
             #print("    ISOMER: " + str(isomer_file))
             if check_educt_to_product(stringfile): # if there is a reaction in the stringfile. make a cut dag!
                 print("        Generate cut dag")
-                #generate_cut_dag_main(stringfile, smiles_path, folder.rsplit("/")[-1])
-                generate_cut_dag_2(stringfile, smiles_path, folder.rsplit("/")[-1], True, True)
+                generate_cut_dag_2(mode, stringfile, smiles_path, folder.rsplit("/")[-1], visual_cut_dag, visual_stringfiles)
             else:
                 print("        Not generating cut dag")
