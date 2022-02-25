2912f3f6d61cd37930c6e50b7a787b75e69594e4
ValentinHoeck <valentin.hoeck@gmail.com>
2022-02-25 12:15:22 +0100

    my main error
    the main will not click together...

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ if __name__ == '__main__':
     if len(argv) == 2:
         # ---------------visualizers---------------visualizers---------------visualizers---------------visualizers---------------
         if str(argv[1]) == "ec":                        # show energy curves for stringfiles of reaction
-            visualize_energy_curves(folder="blackbox/output/CC=CC=CC=CO/reaction0005")
+            visualize_energy_curves(folder="blackbox/output/CC(CCC)=C(CCC)C=CO_NOT_DONE/reaction0017")
         elif str(argv[1]) == "ec_all_reactions":        # show energy curves for all reactions
-            energy_curve_all_reactions(folder="blackbox/output/CC=C")
+            energy_curve_all_reactions(folder="blackbox/output/CC=NC=C(O)C(=CC)CO/reaction0018")
         elif str(argv[1]) == "img_all_stringfiles":     # create image for all stringfiles in folder
-            visualise_stringfiles(overall_folder="blackbox/output/CC(CCC)=CC=CC=CO_04a5")
+            visualise_stringfiles(overall_folder="blackbox/output/CC=CC=CC=CC_e152")
         elif str(argv[1]) == "img_stringfile":          # create image for specific stringfile
             visualize_2D(stringfile_path="blackbox/output/CC(CCC)=C(CCC)C=CO_164c/reaction0001/stringfile.xyz0001", image_path="blackbox/output/CC(CCC)=C(CCC)C=CO_164c/reaction0001")
         elif str(argv[1]) == "make_cut_dag":  # create cut dag for specific stringfile
-            make_cut_dag(blackbox=False, stringfile="blackbox/output/CC=CC=CC=CC/reaction0070/stringfile.xyz0070",visual_cut_dag=True , visual_stringfiles=True, DEBUG_MODE=False)
+            make_cut_dag(blackbox=True, stringfile="blackbox/output/CC=CC=CC=CC/reaction0030/stringfile.xyz0030",visual_cut_dag=True , visual_stringfiles=True, DEBUG_MODE=False)
 
         # ---------------Calc---------------Calc---------------Calc---------------Calc---------------Calc---------------
         elif str(argv[1]) == "smiles_to_reactions_bb":  # Compute all reactions as stringfiles from smiles
-            make_reactions(blackbox=True, string_data=["CC=C"], max_energy=200, debug=True)
+            make_reactions(blackbox=True, string_data=["CC=CC=CC=CC"], max_energy=200, debug=False)
         elif str(argv[1]) == "smiles_to_reactions_bb_multirun":  # Compute all reactions as stringfiles from smiles
             #energy_curves("blackbox/output/CC=CCCCC_sigmatropicForced_multirun")
             for i in range(20):
@@ -43,4 +43,19 @@ if __name__ == '__main__':
         else:
             print("input argument is unknown")
     else:
+        '''
+        from rdkit.Chem import RWMol
+        from src.stringfile_to_rdkit import stringfile_to_rdkit
+        rdk_mol, atom_core, ec = stringfile_to_rdkit("blackbox/output/CC(CCC)=C(CCC)C=CO_NOT_DONE/reaction0017/stringfile.xyz0017")
+        rdk_mol = RWMol(rdk_mol)
+        from src.cut_molecule import make_cut_molecule, make_cut
+        molecule, lookup = make_cut_molecule(rdk_mol, atom_core)
+
+        mol, ordering  = make_cut(rdk_mol, {2,3,4,6,7,8,9,10,11}, molecule, lookup)
+        print(ordering)
+        from src.stringfile_tester import check_product, get_removed_atoms
+        print(check_product("blackbox/output/CC(CCC)=C(CCC)C=CO_NOT_DONE/reaction0017/stringfile.xyz0017", "blackbox/output/CC(CCC)=C(CCC)C=CO_NOT_DONE/reaction0017/2_3_4_6_7_8_9_10_11/stringfile.xyz0000", {2,3,4,6,7,8,9,10,11}, ordering, molecule, lookup, rdk_mol))
+
+        #print(get_removed_atoms({2,3,4,6,7,8,9,10,11}, molecule, lookup, rdk_mol))
+        '''
         print("input argument required")
